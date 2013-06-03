c5b5f4556250ba757f8165851cc1ea1b1f186290
dkolovos@CS.YORK.AC.UK <dkolovos@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-06-03 22:25:32 +0000

    Commented-out GitHub project
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@426 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- apps/org.ossmeter.platform.app.york/src/org/ossmeter/platform/app/york/App.java
+++ apps/org.ossmeter.platform.app.york/src/org/ossmeter/platform/app/york/App.java
@@ -6,6 +6,10 @@ import org.ossmeter.platform.ExtensionPointMetricProviderManager;
 import org.ossmeter.platform.IMetricProviderManager;
 import org.ossmeter.platform.Platform;
 import org.ossmeter.platform.app.york.util.ProjectCreationUtil;
+import org.ossmeter.platform.delta.bugtrackingsystem.ExtensionPointBugTrackingSystemManager;
+import org.ossmeter.platform.delta.bugtrackingsystem.PlatformBugTrackingSystemManager;
+import org.ossmeter.platform.delta.communicationchannel.ExtensionPointCommunicationChannelManager;
+import org.ossmeter.platform.delta.communicationchannel.PlatformCommunicationChannelManager;
 import org.ossmeter.platform.delta.vcs.ExtensionPointVcsManager;
 import org.ossmeter.platform.delta.vcs.PlatformVcsManager;
 import org.ossmeter.repository.model.Project;
@@ -16,7 +20,8 @@ import com.mongodb.Mongo;
 
 public class App implements IApplication {
 	
-	public void run(IMetricProviderManager metricProviderManager, PlatformVcsManager platformVcsManager) throws Exception {
+	public void run(IMetricProviderManager metricProviderManager, PlatformVcsManager platformVcsManager,
+			PlatformCommunicationChannelManager communicationChannelManager, PlatformBugTrackingSystemManager bugTrackingSystemManager) throws Exception {
 		Mongo mongo = new Mongo();
 		
 		PongoFactory.getInstance().getContributors().add(new OsgiPongoFactoryContributor());
@@ -24,20 +29,22 @@ public class App implements IApplication {
 		Platform platform = new Platform(mongo);
 		platform.setMetricProviderManager(metricProviderManager);
 		platform.setPlatformVcsManager(platformVcsManager);
+		platform.setPlatformCommunicationChannelManager(communicationChannelManager);
+		platform.setPlatformBugTrackingSystemManager(bugTrackingSystemManager);
 		
 		// FIXME: Needs to check Mongo for projects, not keep registering the same ones!
 		
 		Project pongo = ProjectCreationUtil.createSvnProject("pongo", "https://pongo.googlecode.com/svn");
 		Project saf = ProjectCreationUtil.createGitProject("saf", "https://code.google.com/p/super-awesome-fighter");
 		Project fedora = ProjectCreationUtil.createProjectWithBugTrackingSystem("fedora", "https://bugzilla.redhat.com/xmlrpc.cgi", "Fedora", "acpi"); // "acpi", platform);
-		Project mojambo = ProjectCreationUtil.createGitHubProject("mojambo", "grit");
+		//Project mojambo = ProjectCreationUtil.createGitHubProject("mojambo", "grit");
 		Project skim = ProjectCreationUtil.createSourceForgeProject("skim-app");
 		Project epsilon = ProjectCreationUtil.createProjectWithNewsGroup("epsilon", "news.eclipse.org", "eclipse.epsilon", true, "exquisitus", "flinder1f7", 80, 10000);
 		
 		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(pongo);
 		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(saf);
 		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(fedora);
-		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(mojambo);
+		//platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(mojambo);
 		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(skim);
 		platform.getProjectRepositoryManager().getProjectRepository().getProjects().add(epsilon);
 		platform.getProjectRepositoryManager().getProjectRepository().sync();
@@ -47,7 +54,7 @@ public class App implements IApplication {
 	
 	@Override
 	public Object start(IApplicationContext context) throws Exception {
-		run(new ExtensionPointMetricProviderManager(), new ExtensionPointVcsManager());
+		run(new ExtensionPointMetricProviderManager(), new ExtensionPointVcsManager(), new ExtensionPointCommunicationChannelManager(), new ExtensionPointBugTrackingSystemManager());
 		return IApplication.EXIT_OK;
 	}
 
