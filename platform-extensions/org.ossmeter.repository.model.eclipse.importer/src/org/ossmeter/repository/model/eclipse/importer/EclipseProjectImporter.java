dd1c06f6973948402de1da846544560d68d7fe7b
davide.diruscio@univaq.it <davide.diruscio@univaq.it@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-09-20 08:51:13 +0000

    Initial Import
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@542 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- platform-extensions/org.ossmeter.repository.model.eclipse.importer/src/org/ossmeter/repository/model/eclipse/importer/EclipseProjectImporter.java
+++ platform-extensions/org.ossmeter.repository.model.eclipse.importer/src/org/ossmeter/repository/model/eclipse/importer/EclipseProjectImporter.java
@@ -1,11 +1,45 @@
 package org.ossmeter.repository.model.eclipse.importer;
 
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.ossmeter.repository.model.eclipse.importer.util.XML;
 import org.ossmeter.repository.model.eclipseforge.EclipseForgeProject;
+import org.w3c.dom.Element;
+
+
 
 public class EclipseProjectImporter {
 	
+	private Logger logger;
+	
+	
 	public EclipseForgeProject importProject(String projectId) {
+		logger=Logger.getLogger(this.getClass().getName());
+	
+		EclipseForgeProject ep = new EclipseForgeProject();
+		String URL_PROJECT = "http://projects.eclipse.org/projects/" + projectId;
+		
+		String HTML = null;
+		
+		ep.setDescriptionUrl(getAttribute("descriptionUrl", HTML));
+		ep.setShortName(getAttribute("shortName", HTML));
+		
+		
+	    return ep;
+	}
+
+
+	public String getAttribute(String name, String HTML){
+		
 		return null;
 	}
+	
+
+	
 	
 }
