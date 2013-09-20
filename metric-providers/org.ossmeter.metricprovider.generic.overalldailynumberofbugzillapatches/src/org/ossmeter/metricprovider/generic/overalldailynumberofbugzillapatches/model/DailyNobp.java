b97a31bddfc19400fa4a76d16334be80322d5574
ioannis.korkontzelos@manchester.ac.uk <ioannis.korkontzelos@manchester.ac.uk@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-09-20 12:59:23 +0000

    Initial import.
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@566 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

---
+++ metric-providers/org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches/src/org/ossmeter/metricprovider/generic/overalldailynumberofbugzillapatches/model/DailyNobp.java
@@ -0,0 +1,32 @@
+package org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches.model;
+
+import java.util.List;
+
+import com.googlecode.pongo.runtime.Pongo;
+import com.googlecode.pongo.runtime.PongoList;
+import com.mongodb.BasicDBList;
+
+
+public class DailyNobp extends Pongo {
+	
+	protected List<DailyBugzillaData> bugzillas = null;
+	
+	
+	public DailyNobp() { 
+		super();
+		dbObject.put("bugzillas", new BasicDBList());
+	}
+	
+	
+	
+	
+	
+	public List<DailyBugzillaData> getBugzillas() {
+		if (bugzillas == null) {
+			bugzillas = new PongoList<DailyBugzillaData>(this, "bugzillas", true);
+		}
+		return bugzillas;
+	}
+	
+	
+}
