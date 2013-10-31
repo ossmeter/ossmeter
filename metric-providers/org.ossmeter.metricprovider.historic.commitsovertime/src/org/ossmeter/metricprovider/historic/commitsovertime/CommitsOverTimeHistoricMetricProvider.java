f97d08934f74b8b56358bd159c1dd70b8b97a962
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-10-31 16:18:31 +0000

    Commits over time metric
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@688 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

---
+++ metric-providers/org.ossmeter.metricprovider.historic.commitsovertime/src/org/ossmeter/metricprovider/historic/commitsovertime/CommitsOverTimeHistoricMetricProvider.java
@@ -0,0 +1,88 @@
+package org.ossmeter.metricprovider.historic.commitsovertime;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.ossmeter.metricprovider.historic.commitsovertime.model.CommitsOverTime;
+import org.ossmeter.metricprovider.trans.commits.CommitsTransientMetricProvider;
+import org.ossmeter.metricprovider.trans.commits.model.Commits;
+import org.ossmeter.metricprovider.trans.commits.model.RepositoryData;
+import org.ossmeter.platform.IHistoricalMetricProvider;
+import org.ossmeter.platform.IMetricProvider;
+import org.ossmeter.platform.MetricProviderContext;
+import org.ossmeter.repository.model.Project;
+
+import com.googlecode.pongo.runtime.Pongo;
+
+public class CommitsOverTimeHistoricMetricProvider implements
+		IHistoricalMetricProvider {
+
+	protected List<IMetricProvider> uses;
+	protected MetricProviderContext context;
+	
+	@Override
+	public String getIdentifier() {
+		return CommitsOverTimeHistoricMetricProvider.class.getCanonicalName();
+	}
+
+	@Override
+	public String getShortIdentifier() {
+		return "Commits over time";
+	}
+
+	@Override
+	public String getFriendlyName() {
+		return "commitsovertime";
+	}
+
+	@Override
+	public String getSummaryInformation() {
+		return "The total number of commits over time.";
+	}
+
+	@Override
+	public boolean appliesTo(Project project) {
+		return project.getVcsRepositories().size() > 0;
+	}
+
+	@Override
+	public void setUses(List<IMetricProvider> uses) {
+		this.uses = uses;
+	}
+
+	@Override
+	public List<String> getIdentifiersOfUses() {
+		return Arrays.asList(CommitsTransientMetricProvider.class.getCanonicalName());
+	}
+
+	@Override
+	public void setMetricProviderContext(MetricProviderContext context) {
+		this.context = context;
+	}
+
+	@Override
+	public Pongo measure(Project project) {
+		
+		List<RepositoryData> repos = new ArrayList<RepositoryData>();
+		
+		for (IMetricProvider used : uses) {
+			Commits usedCommits =  ((CommitsTransientMetricProvider)used).adapt(context.getProjectDB(project));
+			for (RepositoryData rd : usedCommits.getRepositories()) {
+				repos.add(rd);
+			}
+		}
+		
+		CommitsOverTime cot = new CommitsOverTime();
+		
+		for (RepositoryData rd : repos) {
+			org.ossmeter.metricprovider.historic.commitsovertime.model.RepositoryData histRd = new org.ossmeter.metricprovider.historic.commitsovertime.model.RepositoryData();
+			histRd.setUrl(rd.getUrl());
+			histRd.setNumberOfCommits(rd.getTotalCommits());
+			cot.getRepositories().add(histRd);
+		}
+		
+		return cot;
+	}
+
+}
