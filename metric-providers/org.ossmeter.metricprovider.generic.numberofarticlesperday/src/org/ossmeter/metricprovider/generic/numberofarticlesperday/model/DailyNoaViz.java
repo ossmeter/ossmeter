d47a45d23bfe0921fdf1c557fa41e8b4b9eb1e7d
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-05 20:05:23 +0000

    Visualisation for articles per day
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@717 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.numberofarticlesperday/src/org/ossmeter/metricprovider/generic/numberofarticlesperday/model/DailyNoaViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.numberofarticlesperday/src/org/ossmeter/metricprovider/generic/numberofarticlesperday/model/DailyNoaViz.java
@@ -2,7 +2,8 @@
 package org.ossmeter.metricprovider.generic.numberofarticlesperday.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
-import com.mongodb.DBCollection;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
 
 public class DailyNoaViz extends PongoViz {
 
@@ -10,16 +11,31 @@ public class DailyNoaViz extends PongoViz {
 		super();
 	}
 	
-	public DailyNoaViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.numberofarticlesperday");
 	}
 
 	@Override
 	public String getViz(String type) {
 		switch (type) {
 			case "gcharts": 
-	String dataTable = createDataTable("newsgroups", "", "__date", "numberOfArticles");
-	return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new bugs' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		}
+				String dataTable = createDataTable("newsgroups", "", "__date", "numberOfArticles");
+				return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new bugs' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");
+			case "d3":
+				return ("{ 'id' : 'articles', 'name' : 'Articles/day', 'type' : 'line', " +
+						"'description' : 'This metric shows the number of articles posted in the newgroup(s) per day.', " +
+						"'xtext' : 'Date', 'ytext':'Articles', 'orderRule' : 'Date', 'datatable' : " + createD3DataTable("newsgroups", "_id", "__date", "numberOfArticles", "Date", "Articles", DateFilter.DAY) + "," +
+						"'isTimeSeries':true, 'lastValue': '"+getLastValue()+"' }").replaceAll("'", "\"");
+			}
 		return null;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Mongo mongo = new Mongo();
+		DB db = mongo.getDB("Subversive");
+			
+		DailyNoaViz viz = new DailyNoaViz();
+		viz.setProjectDB(db);
+		System.err.println(viz.getViz("d3"));
 	}
 }
