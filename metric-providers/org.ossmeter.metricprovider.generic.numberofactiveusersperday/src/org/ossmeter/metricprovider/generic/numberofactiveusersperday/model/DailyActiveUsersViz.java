222e0e5821955b9b57bf5f906a6c099b55816fa0
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-05 20:05:01 +0000

    Visualisation for daily active users metric
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@716 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.numberofactiveusersperday/src/org/ossmeter/metricprovider/generic/numberofactiveusersperday/model/DailyActiveUsersViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.numberofactiveusersperday/src/org/ossmeter/metricprovider/generic/numberofactiveusersperday/model/DailyActiveUsersViz.java
@@ -2,7 +2,8 @@
 package org.ossmeter.metricprovider.generic.numberofactiveusersperday.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
-import com.mongodb.DBCollection;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
 
 public class DailyActiveUsersViz extends PongoViz {
 
@@ -10,16 +11,34 @@ public class DailyActiveUsersViz extends PongoViz {
 		super();
 	}
 	
-	public DailyActiveUsersViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.numberofactiveusersperday");
 	}
 
 	@Override
 	public String getViz(String type) {
 		switch (type) {
 			case "gcharts": 
-	String dataTable = createDataTable("newsgroups", "", "__date", "numberOfActiveUsers");
-	return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new active users' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		}
+				String dataTable = createDataTable("newsgroups", "", "__date", "numberOfActiveUsers");
+				return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new active users' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		
+			case "d3":
+				return ("{ 'id' : 'activeusersperday', 'name' : 'Active Users', 'type' : 'line', " +
+						"'description' : 'A measurement of the number of active daily users of the newsgroup.', " +
+						"'xtext' : 'Date', 'ytext':'Active Users', 'orderRule' : 'Date', 'datatable' : " + createD3DataTable("newsgroups", "_id", "__date", "numberOfActiveUsers", "Date", "Active Users", DateFilter.DAY) + "," +
+						"'isTimeSeries':true, 'lastValue': '"+getLastValue()+"' }").replaceAll("'", "\"");
+		}
+		
+		
 		return null;
 	}
+
+	public static void main(String[] args) throws Exception {
+		Mongo mongo = new Mongo();
+		DB db = mongo.getDB("Subversive");
+			
+		DailyActiveUsersViz viz = new DailyActiveUsersViz();
+		viz.setProjectDB(db);
+		System.err.println(viz.getViz("d3"));
+	}
+
 }
