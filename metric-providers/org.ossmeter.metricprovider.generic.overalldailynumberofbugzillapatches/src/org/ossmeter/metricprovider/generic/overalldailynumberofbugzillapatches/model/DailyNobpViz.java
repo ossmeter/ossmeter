10d143f511eeb871521b6b963a243f0071f3c90e
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-05 20:07:12 +0000

    Viz for bugzilla patches
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@721 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches/src/org/ossmeter/metricprovider/generic/overalldailynumberofbugzillapatches/model/DailyNobpViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches/src/org/ossmeter/metricprovider/generic/overalldailynumberofbugzillapatches/model/DailyNobpViz.java
@@ -2,7 +2,8 @@
 package org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
-import com.mongodb.DBCollection;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
 
 public class DailyNobpViz extends PongoViz {
 
@@ -10,16 +11,27 @@ public class DailyNobpViz extends PongoViz {
 		super();
 	}
 	
-	public DailyNobpViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.overalldailynumberofbugzillapatches");
 	}
 
 	@Override
 	public String getViz(String type) {
 		switch (type) {
-			case "gcharts": 
-	String dataTable = createDataTable("bugzillas", "", "__date", "numberOfPatches");
-	return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new patches' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		}
+			case "d3":
+				return ("{ 'id' : 'dailybugzillapatches', 'name' : 'Bug patches/day', 'type' : 'bar', " +
+						"'description' : 'A measure of the number of bug comments submitted by day.', " +
+						"'xtext' : 'Date', 'ytext':'Patches', 'orderRule' : 'Date', 'datatable' : " + createD3DataTable("bugzillas", "_id", "__date", "numberOfPatches", "Date", "Patches", DateFilter.DAY) + "," +
+						"'isTimeSeries':true, 'lastValue': '"+getLastValue()+"' }").replaceAll("'", "\"");
+		}
 		return null;
+	}
+	public static void main(String[] args) throws Exception {
+		Mongo mongo = new Mongo();
+		DB db = mongo.getDB("Subversive");
+			
+		DailyNobpViz viz = new DailyNobpViz();
+		viz.setProjectDB(db);
+		System.err.println(viz.getViz("d3"));
 	}
 }
