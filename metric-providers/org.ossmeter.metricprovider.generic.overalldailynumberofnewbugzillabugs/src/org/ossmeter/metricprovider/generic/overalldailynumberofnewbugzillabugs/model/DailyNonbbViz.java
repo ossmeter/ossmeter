b789757df0e61f0e0b35f6350e5f286eeb5a5cca
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-05 20:07:25 +0000

    Viz for new bugs
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@722 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.overalldailynumberofnewbugzillabugs/src/org/ossmeter/metricprovider/generic/overalldailynumberofnewbugzillabugs/model/DailyNonbbViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.overalldailynumberofnewbugzillabugs/src/org/ossmeter/metricprovider/generic/overalldailynumberofnewbugzillabugs/model/DailyNonbbViz.java
@@ -2,7 +2,8 @@
 package org.ossmeter.metricprovider.generic.overalldailynumberofnewbugzillabugs.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
-import com.mongodb.DBCollection;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
 
 public class DailyNonbbViz extends PongoViz {
 
@@ -10,16 +11,30 @@ public class DailyNonbbViz extends PongoViz {
 		super();
 	}
 	
-	public DailyNonbbViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.overalldailynumberofnewbugzillabugs");
 	}
 
 	@Override
 	public String getViz(String type) {
 		switch (type) {
 			case "gcharts": 
-	String dataTable = createDataTable("bugzillas", "", "__date", "numberOfBugs");
-	return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new bugs' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		}
+				String dataTable = createDataTable("bugzillas", "", "__date", "numberOfBugs");
+				return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Number of new bugs' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");	
+			case "d3":
+				return ("{ 'id' : 'dailynewbugzillabugs', 'name' : 'New bugs/day', 'type' : 'bar', " +
+						"'description' : 'The number of new bug patches committed per day.', " +
+						"'xtext' : 'Date', 'ytext':'Bugs', 'orderRule' : 'Date', 'datatable' : " + createD3DataTable("bugzillas", "_id", "__date", "numberOfBugs", "Date", "Bugs", DateFilter.DAY) + "," +
+						"'isTimeSeries':true, 'lastValue': '"+getLastValue()+"' }").replaceAll("'", "\"");
+		}
 		return null;
+	}
+	public static void main(String[] args) throws Exception {
+		Mongo mongo = new Mongo();
+		DB db = mongo.getDB("Tomcat");
+			
+		DailyNonbbViz viz = new DailyNonbbViz();
+		viz.setProjectDB(db);
+		System.err.println(viz.getViz("d3"));
 	}
 }
