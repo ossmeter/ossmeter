6f1bdfabea0ebe89768bcfbf532a3d9464d1739d
a.shahi@cwi.nl <a.shahi@cwi.nl@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-13 03:25:42 +0000

    Made changes so that the metric provider is transient.
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@735 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.rascal.examples/src/org/ossmeter/metricprovider/rascal/metric/LinesOfCode.java
+++ metric-providers/org.ossmeter.metricprovider.rascal.examples/src/org/ossmeter/metricprovider/rascal/metric/LinesOfCode.java
@@ -1,46 +1,100 @@
 package org.ossmeter.metricprovider.rascal.metric;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+
 import org.ossmeter.metricprovider.rascal.RascalMetrics;
+import org.ossmeter.metricprovider.rascal.RascalManager;
 import org.eclipse.imp.pdb.facts.IInteger;
 import org.eclipse.imp.pdb.facts.ITuple;
 import org.eclipse.imp.pdb.facts.IValue;
 import org.ossmeter.metricprovider.rascal.metric.trans.model.LinesOfCodeData;
 import org.ossmeter.metricprovider.rascal.metric.trans.model.Loc;
-import org.ossmeter.platform.Date;
-import org.rascalmpl.interpreter.Evaluator;
-import org.rascalmpl.interpreter.NullRascalMonitor;
-
+import org.ossmeter.platform.IMetricProvider;
+import org.ossmeter.platform.ITransientMetricProvider;
+import org.ossmeter.platform.MetricProviderContext;
+import org.ossmeter.platform.delta.ProjectDelta;
+import org.ossmeter.repository.model.Project;
 import com.mongodb.DB;
 
-public class LinesOfCode extends RascalMetrics {
-	private Loc db;
+public class LinesOfCode extends RascalMetrics implements ITransientMetricProvider<Loc> {
 	
 	public LinesOfCode() {
 		this.module = "LOC";
 		this.function = "countLoc";
+		RascalManager.importModule(this.module);
 	}
 	
-	public void adapt(DB db) {
-		this.db = new Loc(db);
-	}
-
-	public void measure(Evaluator eval, IValue file, String revision, String fileURL, Date today) {
-		ITuple result = (ITuple) eval.call(new NullRascalMonitor(), module, function, file);
-		LinesOfCodeData locData = db.getLinesOfCode().findOneByFile(fileURL);//new LinesOfCodeData();
-		if (locData == null) {
-			locData = new LinesOfCodeData();
-		}
-		locData.setFile(fileURL);
-		locData.setRevisionNumber(revision);
-		locData.setTotalLines(((IInteger)result.get(0)).longValue());
-		locData.setCommentedLines(((IInteger)result.get(1)).longValue());
-		locData.setEmptyLines(((IInteger)result.get(2)).longValue());
-		locData.setSourceLines(((IInteger)result.get(3)).longValue());
-		db.getLinesOfCode().add(locData);
+	public Loc adapt(DB db) {
+		return new Loc(db);
 	}
 
 	@Override
-	public void sync() {
-		this.db.sync();
+	public String getIdentifier() {
+		return LinesOfCode.class.getCanonicalName();
+	}
+
+	@Override
+	public String getShortIdentifier() {
+		return "LOC";
+	}
+
+	@Override
+	public String getFriendlyName() {
+		return "Lines Of Code";
+	}
+
+	@Override
+	public String getSummaryInformation() {
+		return "Counts physical, commented, empty and source lines of code per file";
+	}
+
+	@Override
+	public boolean appliesTo(Project project) {
+		return project.getVcsRepositories().size() > 0;
+	}
+
+	@Override
+	public void setUses(List<IMetricProvider> uses) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> getIdentifiersOfUses() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public void setMetricProviderContext(MetricProviderContext context) {
+		this.context = context;
+	}
+
+	@Override
+	public void measure(Project project, ProjectDelta delta, Loc db) {
+		manager = RascalManager.getInstance(project.getName());
+		
+		TreeMap<String, HashMap<String, IValue>> revisions = super.getM3s(project, delta);
+		for (String rev : revisions.keySet()) {
+			HashMap<String, IValue> fileM3 = revisions.get(rev);
+			for (String fileURL : fileM3.keySet()) {
+				ITuple result = (ITuple) manager.callRascal(module, function, new IValue[] {fileM3.get(fileURL)}); 
+				LinesOfCodeData locData = db.getLinesOfCode().findOneByFile(fileURL);//new LinesOfCodeData();
+				if (locData == null) {
+					locData = new LinesOfCodeData();
+				}
+				locData.setFile(fileURL);
+				locData.setDate(delta.getDate().toString());
+				locData.setRevisionNumber(rev);
+				locData.setTotalLines(((IInteger)result.get(0)).longValue());
+				locData.setCommentedLines(((IInteger)result.get(1)).longValue());
+				locData.setEmptyLines(((IInteger)result.get(2)).longValue());
+				locData.setSourceLines(((IInteger)result.get(3)).longValue());
+				db.getLinesOfCode().add(locData);
+			}
+			db.sync();
+		}
 	}
 }
