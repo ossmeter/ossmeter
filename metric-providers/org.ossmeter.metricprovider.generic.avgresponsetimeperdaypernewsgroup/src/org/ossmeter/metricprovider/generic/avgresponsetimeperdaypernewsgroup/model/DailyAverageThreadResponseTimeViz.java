a943222e26779719004a61c368adf56cdef751a6
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2013-11-27 16:02:06 +0000

    Viz for avgresponsetimeperday. Demo.
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@770 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.avgresponsetimeperdaypernewsgroup/src/org/ossmeter/metricprovider/generic/avgresponsetimeperdaypernewsgroup/model/DailyAverageThreadResponseTimeViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.avgresponsetimeperdaypernewsgroup/src/org/ossmeter/metricprovider/generic/avgresponsetimeperdaypernewsgroup/model/DailyAverageThreadResponseTimeViz.java
@@ -1,7 +1,8 @@
- 
 package org.ossmeter.metricprovider.generic.avgresponsetimeperdaypernewsgroup.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
+import com.googlecode.pongo.runtime.viz.PongoViz.DateFilter;
+import com.mongodb.DB;
 import com.mongodb.DBCollection;
 
 public class DailyAverageThreadResponseTimeViz extends PongoViz {
@@ -10,16 +11,17 @@ public class DailyAverageThreadResponseTimeViz extends PongoViz {
 		super();
 	}
 	
-	public DailyAverageThreadResponseTimeViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.avgresponsetimeperdaypernewsgroup");
 	}
 
 	@Override
 	public String getViz(String type) {
 		switch (type) {
 			case "gcharts": 
-	String dataTable = createDataTable("newsgroups", "", "__date", "avgResponseTime");
-	return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Average response time' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		}
+				String dataTable = createDataTable("newsgroups", "", "__date", "avgResponseTime");
+				return ("{ 'target' : 'gcharts', 'chartType' : 'LineChart', 'options' : { 'hAxis' : {'title':'Date'}, 'vAxis':{'title' : 'Average response time' }}, 'datatable' : " + dataTable + " }").replaceAll("'", "\"");		
+		}
 		return null;
 	}
 }
