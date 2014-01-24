3c77bcda037d467b30f64e028d8d658a062b6c36
ioannis.korkontzelos@manchester.ac.uk <ioannis.korkontzelos@manchester.ac.uk@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2014-01-24 13:24:47 +0000

    corrected errors in the Viz file
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@790 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- metric-providers/org.ossmeter.metricprovider.generic.avgbugcreationclosedtime/src/org/ossmeter/metricprovider/generic/avgbugcreationclosedtime/model/DailyAvgBugOpenTimeViz.java
+++ metric-providers/org.ossmeter.metricprovider.generic.avgbugcreationclosedtime/src/org/ossmeter/metricprovider/generic/avgbugcreationclosedtime/model/DailyAvgBugOpenTimeViz.java
@@ -2,7 +2,7 @@
 package org.ossmeter.metricprovider.generic.avgbugcreationclosedtime.model;
 
 import com.googlecode.pongo.runtime.viz.PongoViz;
-import com.mongodb.DBCollection;
+import com.mongodb.DB;
 
 public class DailyAvgBugOpenTimeViz extends PongoViz {
 
@@ -10,8 +10,8 @@ public class DailyAvgBugOpenTimeViz extends PongoViz {
 		super();
 	}
 	
-	public DailyAvgBugOpenTimeViz(DBCollection collection) {
-		super(collection);
+	public void setProjectDB(DB projectDB) {
+		this.collection = projectDB.getCollection("org.ossmeter.metricprovider.generic.avgbugcreationclosedtime");
 	}
 
 	@Override
