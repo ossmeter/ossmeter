3820aaca0e4e5fe9c5ad9cb4864f8dff11a2f342
davide.diruscio@univaq.it <davide.diruscio@univaq.it@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2014-02-09 18:29:09 +0000

    Minors
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@845 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

---
+++ platform-extensions/org.ossmeter.repository.model.vcs.git/src/org/ossmeter/repository/model/vcs/git/GitRepository.java
@@ -0,0 +1,46 @@
+package org.ossmeter.repository.model.vcs.git;
+
+import com.mongodb.*;
+import java.util.*;
+import com.googlecode.pongo.runtime.*;
+import com.googlecode.pongo.runtime.querying.*;
+
+
+public class GitRepository extends org.ossmeter.repository.model.VcsRepository {
+	
+	
+	
+	public GitRepository() { 
+		super();
+		super.setSuperTypes("org.ossmeter.repository.model.vcs.git.VcsRepository");
+		USERNAME.setOwningType("org.ossmeter.repository.model.vcs.git.GitRepository");
+		PASSWORD.setOwningType("org.ossmeter.repository.model.vcs.git.GitRepository");
+	}
+	
+	public static StringQueryProducer USERNAME = new StringQueryProducer("username"); 
+	public static StringQueryProducer PASSWORD = new StringQueryProducer("password"); 
+	
+	
+	public String getUsername() {
+		return parseString(dbObject.get("username")+"", "");
+	}
+	
+	public GitRepository setUsername(String username) {
+		dbObject.put("username", username);
+		notifyChanged();
+		return this;
+	}
+	public String getPassword() {
+		return parseString(dbObject.get("password")+"", "");
+	}
+	
+	public GitRepository setPassword(String password) {
+		dbObject.put("password", password);
+		notifyChanged();
+		return this;
+	}
+	
+	
+	
+	
+}
