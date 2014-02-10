012c2fa07fe25ac9ce04adc3e8e3b979c78ec533
davide.diruscio@univaq.it <davide.diruscio@univaq.it@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2014-02-10 12:41:58 +0000

    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@850 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

---
+++ platform-extensions/org.ossmeter.repository.model.vcs.cvs/src/org/ossmeter/repository/model/vcs/cvs/CvsRepository.java
@@ -0,0 +1,68 @@
+package org.ossmeter.repository.model.vcs.cvs;
+
+import com.mongodb.*;
+import java.util.*;
+import com.googlecode.pongo.runtime.*;
+import com.googlecode.pongo.runtime.querying.*;
+
+
+public class CvsRepository extends org.ossmeter.repository.model.VcsRepository {
+	
+	
+	
+	public CvsRepository() { 
+		super();
+		super.setSuperTypes("org.ossmeter.repository.model.vcs.cvs.VcsRepository");
+		BROWSE.setOwningType("org.ossmeter.repository.model.vcs.cvs.CvsRepository");
+		USERNAME.setOwningType("org.ossmeter.repository.model.vcs.cvs.CvsRepository");
+		PASSWORD.setOwningType("org.ossmeter.repository.model.vcs.cvs.CvsRepository");
+		PATH.setOwningType("org.ossmeter.repository.model.vcs.cvs.CvsRepository");
+	}
+	
+	public static StringQueryProducer BROWSE = new StringQueryProducer("browse"); 
+	public static StringQueryProducer USERNAME = new StringQueryProducer("username"); 
+	public static StringQueryProducer PASSWORD = new StringQueryProducer("password"); 
+	public static StringQueryProducer PATH = new StringQueryProducer("path"); 
+	
+	
+	public String getBrowse() {
+		return parseString(dbObject.get("browse")+"", "");
+	}
+	
+	public CvsRepository setBrowse(String browse) {
+		dbObject.put("browse", browse);
+		notifyChanged();
+		return this;
+	}
+	public String getUsername() {
+		return parseString(dbObject.get("username")+"", "");
+	}
+	
+	public CvsRepository setUsername(String username) {
+		dbObject.put("username", username);
+		notifyChanged();
+		return this;
+	}
+	public String getPassword() {
+		return parseString(dbObject.get("password")+"", "");
+	}
+	
+	public CvsRepository setPassword(String password) {
+		dbObject.put("password", password);
+		notifyChanged();
+		return this;
+	}
+	public String getPath() {
+		return parseString(dbObject.get("path")+"", "");
+	}
+	
+	public CvsRepository setPath(String path) {
+		dbObject.put("path", path);
+		notifyChanged();
+		return this;
+	}
+	
+	
+	
+	
+}
