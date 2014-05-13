106caa3c70abd244991cb5357e9f3c0844605a88
jw@CS.YORK.AC.UK <jw@CS.YORK.AC.UK@1b171192-1bc4-4300-84b6-1b6fde2e8ce3>
2014-05-13 13:54:39 +0000

    Tidied up York app, in preparation for Git migration.
    git-svn-id: https://svn.cs.york.ac.uk/svn/ossmeter/Software/source@1206 1b171192-1bc4-4300-84b6-1b6fde2e8ce3

--- apps/org.ossmeter.platform.app.york/src/org/ossmeter/platform/app/york/Models2014Setup.java
+++ apps/org.ossmeter.platform.app.york/src/org/ossmeter/platform/app/york/Models2014Setup.java
@@ -16,9 +16,10 @@ import com.mongodb.ServerAddress;
 public class Models2014Setup {
 	public static void main(String[] args) throws Exception {
 		List<ServerAddress> addrs = new ArrayList<>();
-		addrs.add(new ServerAddress("144.32.156.177", 27017));
-		addrs.add(new ServerAddress("144.32.156.148", 27017));
-		addrs.add(new ServerAddress("144.32.156.184", 27017));
+//		addrs.add(new ServerAddress("144.32.156.177", 27017));
+//		addrs.add(new ServerAddress("144.32.156.148", 27017));
+//		addrs.add(new ServerAddress("144.32.156.184", 27017));
+		addrs.add(new ServerAddress("localhost", 27017));
 		Mongo mongo = new Mongo(addrs);
 		
 		Platform platform = new Platform(mongo);
@@ -39,50 +40,100 @@ public class Models2014Setup {
 //		projects.findOneByShortName("modeling.gmp.graphiti");
 	
 		
-		updateInformation(projects, "modeling.emf", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "EMF", Arrays.asList("Core"), 
-				"eclipse.tools.emf", "news.eclipse.org/eclipse.tools.emf");
+//		updateInformation(projects, "modeling.emf", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "EMF", Arrays.asList("Core"), 
+//				"eclipse.tools.emf", "news.eclipse.org/eclipse.tools.emf");
+//		
+//		updateInformation(projects, "modeling.mmt.atl", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("ATL"), 
+//				"eclipse.atl", "news.eclipse.org/eclipse.atl");
+//		
+//		updateInformation(projects, "modeling.epsilon", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "Epsilon", Arrays.asList("Core"), 
+//				"eclipse.epsilon", "news.eclipse.org/eclipse.epsilon");
+//		
+//		updateInformation(projects, "modeling.gmp.gmf-runtime", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMF-Runtime", Arrays.asList("General"), 
+//				"eclipse.modeling.gmf", "news.eclipse.org/eclipse.modeling.gmf");
+//		
+//		updateInformation(projects, "modeling.tmf.xtext", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "TMF", Arrays.asList("Xtext"), 
+//				"eclipse.modeling.tmf", "news.eclipse.org/eclipse.modeling.tmf");
+//		
+//		updateInformation(projects, "modeling.viatra2", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("VIATRA2"), 
+//				"eclipse.viatra2", "news.eclipse.org/eclipse.viatra2");
+//		
+//		updateInformation(projects, "modeling.gmt.amw", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("AMW"), 
+//				"eclipse.amw", "news.eclipse.org/eclipse.amw");
+//		
+//		updateInformation(projects, "modeling.mdt.papyrus", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "MDT.Papyrus", Arrays.asList("Core", "Diagram", "Others", "Table", "Views"), 
+//				"eclipse.papyrus", "news.eclipse.org/eclipse.papyrus");
 		
-		updateInformation(projects, "modeling.mmt.atl", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("ATL"), 
-				"eclipse.atl", "news.eclipse.org/eclipse.atl");
+//		updateInformation(projects, "modeling.mdt.modisco", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "MDT.MoDisco", Arrays.asList("Main", "Contribution", "Infrastructure", "Technologies", "UseCases"), 
+//				"eclipse.modisco", "news.eclipse.org/eclipse.modisco");
 		
-		updateInformation(projects, "modeling.epsilon", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "Epsilon", Arrays.asList("Core"), 
-				"eclipse.epsilon", "news.eclipse.org/eclipse.epsilon");
+//		createProject(projects, "eclipse.platform", 
+//				"", "", Arrays.asList(""), 
+//				"eclipse.platform", "news.eclipse.org/eclipse.platform");
+
+//		createProject(projects, "modeling.mdt.xsd", 
+//				"", "", Arrays.asList(""), 
+//				"modeling.mdt.xsd", "news.eclipse.org/eclipse.technology.xsd");
 		
-		updateInformation(projects, "modeling.gmp.gmf-runtime", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMF-Runtime", Arrays.asList("General"), 
-				"eclipse.modeling.gmf", "news.eclipse.org/eclipse.modeling.gmf");
+//		createProject(projects, "modeling.emf", 
+//				"", "", Arrays.asList(""), 
+//				"eclipse.tools.emf", "news.eclipse.org/eclipse.tools.emf");
 		
-		updateInformation(projects, "modeling.tmf.xtext", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "TMF", Arrays.asList("Xtext"), 
-				"eclipse.modeling.tmf", "news.eclipse.org/eclipse.modeling.tmf");
+//		createProject(projects, "eclipse.modeling.m2t", 
+//				"", "", Arrays.asList(""), 
+//				"eclipse.modeling.m2t", "news.eclipse.org/eclipse.modeling.m2t");
+//		
+//		createProject(projects, "eclipse.technology.emft", 
+//				"", "", Arrays.asList(""), 
+//				"eclipse.technology.emft", "news.eclipse.org/eclipse.technology.emft");
 		
-		updateInformation(projects, "modeling.viatra2", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("VIATRA2"), 
-				"eclipse.viatra2", "news.eclipse.org/eclipse.viatra2");
-		
-		updateInformation(projects, "modeling.gmt.amw", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMT", Arrays.asList("AMW"), 
-				"eclipse.amw", "news.eclipse.org/eclipse.amw");
-		
-		updateInformation(projects, "modeling.mdt.papyrus", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "MDT.Papyrus", Arrays.asList("Core", "Diagram", "Others", "Table", "Views"), 
-				"eclipse.papyrus", "news.eclipse.org/eclipse.papyrus");
-		
-		updateInformation(projects, "modeling.mdt.modisco", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "MDT.MoDisco", Arrays.asList("Main", "Contribution", "Infrastructure", "Technologies", "UseCases"), 
-				"eclipse.modisco", "news.eclipse.org/eclipse.modisco");
+		createProject(projects, "modeling.mdt.uml2", 
+				"", "", Arrays.asList(""), 
+				"modeling.mdt.uml2", "news.eclipse.org/eclipse.modeling.mdt.uml2");
 		
 //		updateInformation(projects, "modeling.emfcompare", 
 //				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "EMFCompare", Arrays.asList("Core","Diagram","Team","UI"), 
 //				"eclipse.modeling.", "news.eclipse.org/eclipse.atl");
 		
-		updateInformation(projects, "modeling.gmp.graphiti", 
-				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMP", Arrays.asList("Graphiti"), 
-				"eclipse.graphiti", "news.eclipse.org/eclipse.graphiti");
+//		updateInformation(projects, "modeling.gmp.graphiti", 
+//				"https://bugs.eclipse.org/bugs/xmlrpc.cgi", "GMP", Arrays.asList("Graphiti"), 
+//				"eclipse.graphiti", "news.eclipse.org/eclipse.graphiti");
 		
+		projects.sync();
+	}
+	
+	protected static void createProject(ProjectCollection projects, String projectName, 
+			String bugzillaUrl, String bugzillaProduct, List<String> bugzillaComponents, 
+			String nntpName, String nntpUrl) {
+		Project project = new Project();
+		project.setName(projectName.replace(".", ""));
+		project.setShortName(projectName);
+		project.setLastExecuted("");
+		project.getMetricProviderData().clear();
+
+		// NNTP
+		project.getCommunicationChannels().clear();
+		
+		NntpNewsGroup nntp = new NntpNewsGroup();
+		nntp.setName(nntpName);
+		nntp.setUrl(nntpUrl);
+		nntp.setAuthenticationRequired(true);
+		nntp.setUsername("exquisitus");
+		nntp.setPassword("flinder1f7");
+		nntp.setPort(80);
+		nntp.setLastArticleChecked("-1");
+		project.getCommunicationChannels().add(nntp);
+		
+		projects.add(project);
 		projects.sync();
 	}
 
@@ -105,14 +156,14 @@ public class Models2014Setup {
 		// BUGZILLA
 		project.getBugTrackingSystems().clear();
 
-//		for (String comp : bugzillaComponents) {
-//			Bugzilla bugzilla = new Bugzilla();
-//			bugzilla.setUrl(bugzillaUrl);
-//			bugzilla.setProduct(bugzillaProduct);
-//			bugzilla.setComponent(comp);
-//			
-//			project.getBugTrackingSystems().add(bugzilla);
-//		}  // FIXME: Removed temporarily for performance reasons
+		for (String comp : bugzillaComponents) {
+			Bugzilla bugzilla = new Bugzilla();
+			bugzilla.setUrl(bugzillaUrl);
+			bugzilla.setProduct(bugzillaProduct);
+			bugzilla.setComponent(comp);
+			
+			project.getBugTrackingSystems().add(bugzilla);
+		}  // FIXME: Removed temporarily for performance reasons
 		
 		// NNTP
 		project.getCommunicationChannels().clear();
