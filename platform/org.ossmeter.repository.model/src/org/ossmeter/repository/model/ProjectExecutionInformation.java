477d21d09cd51594164de09d84d97452d0bb2532
James Williams <jamesrobertwilliams@gmail.com>
2014-06-26 19:23:52 +0100

    Updates to the OSSMETER project metamodel, in response to bug fixing in Pongo. Edited the way scheduling information is stored and set boolean defaults.

--- platform/org.ossmeter.repository.model/src/org/ossmeter/repository/model/ProjectExecutionInformation.java
+++ platform/org.ossmeter.repository.model/src/org/ossmeter/repository/model/ProjectExecutionInformation.java
@@ -14,6 +14,7 @@ public class ProjectExecutionInformation extends Pongo {
 	
 	public ProjectExecutionInformation() { 
 		super();
+		dbObject.put("storage", new BasicDBObject());
 		dbObject.put("metricProviderData", new BasicDBList());
 		LASTEXECUTED.setOwningType("org.ossmeter.repository.model.ProjectExecutionInformation");
 		MONITOR.setOwningType("org.ossmeter.repository.model.ProjectExecutionInformation");
@@ -65,6 +66,7 @@ public class ProjectExecutionInformation extends Pongo {
 	public LocalStorage getStorage() {
 		if (storage == null && dbObject.containsField("storage")) {
 			storage = (LocalStorage) PongoFactory.getInstance().createPongo((DBObject) dbObject.get("storage"));
+			storage.setContainer(this);
 		}
 		return storage;
 	}
