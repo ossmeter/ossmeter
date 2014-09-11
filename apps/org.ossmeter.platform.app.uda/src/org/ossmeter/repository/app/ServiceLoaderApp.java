5c166126f12a040a797844fbcdff06e44a80c8c7
md2manoppello <juri.dirocco@uniaq.it>
2014-09-11 17:08:43 +0200

    Add uda test project.

---
+++ apps/org.ossmeter.platform.app.uda/src/org/ossmeter/repository/app/ServiceLoaderApp.java
@@ -0,0 +1,14 @@
+package org.ossmeter.repository.app;
+
+import org.ossmeter.platform.delta.vcs.ServiceVcsManager;
+
+public class ServiceLoaderApp {
+
+	public static void main(String[] args) throws Exception {
+//		ServiceLoaderSetup.copyMetaInfToBins();
+
+		ServiceLoaderMetricProviderManager mpManager = new ServiceLoaderMetricProviderManager();
+	
+		new App().run(mpManager, new ServiceVcsManager());
+	}
+}
