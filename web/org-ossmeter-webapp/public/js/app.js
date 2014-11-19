bf8ed117b611c9a447c6ff90bd8b621dc6f62d58
James Williams <jamesrobertwilliams@gmail.com>
2014-11-19 02:51:00 +0000

    More work on quality-model-driven presentation. Nearly there. Project view is broken down into types - summary and detail (each able to be driven by different QMs). Generalise spark table drawing.

--- web/org-ossmeter-webapp/public/js/app.js
+++ web/org-ossmeter-webapp/public/js/app.js
@@ -90,6 +90,59 @@ function toggleSpark(elem, projectid, projectname, metricid, metricname) {
 		});
 }
 
+function drawSparkTable(tableId, projectId, metrics, querystring, drawWatches) {
+    $.getJSON("http://localhost:8182/projects/p/" + projectId + "/s/" + metrics + "?" + querystring, function (result) {
+        // Convert into an array if only one spark was requested
+        if( Object.prototype.toString.call( result ) === '[object Object]' ) {
+            result = [result];
+        }
+
+        // Draw a new row for each spark
+        for (var r in result) {
+                var data = result[r];
+
+                // FIXME: If the first sparkle is in error, this won't work.
+                if (r == 0) { // Set up the header
+                    $("#" + tableId + " > thead:last").append(
+                        "<tr><th></th><th>metric</th>" +
+                        "<th>" + data.firstDate + "</th>" +
+                        "<th>" + data.months + " months</th>" +
+                        "<th>" + data.lastDate + "</th>" +
+                        "<th>low</th>" +
+                        "<th>high</th></tr>");
+                }
+
+                if (data.status === "error") {
+                    console.log("Unable to load sparky '" + data.metricId + "': " + data.msg);
+                    continue;
+                }
+
+                console.log("drawing " + data.metricId)
+
+                var a = ""; 
+                if ($.inArray(data.name, app.grid.sparks) != -1){
+                    a = "active";
+                }
+                var toAppend = "<tr><td>";
+
+                if (drawWatches && app.loggedIn) {
+                    var id = "watch-spark-@project.getShortName()-" + data.name;
+                    toAppend = toAppend + '<a href="javascript:toggleSpark(\'#'+id+'\',\'@project.getShortName()\',\'@project.getName()\',\''+ data.name + '\', \''+data.name+'\')"><span id="' + id + '" class="glyphicon glyphicon-eye-open spark-watch tip ' + a + '" data-toggle="tooltip" data-placement="left" title="Watch/unwatch on dashboard"></span></a>';
+                }
+               toAppend = toAppend + "</td>" +
+                    "<td>" + data.name + 
+                    "</td><td>" + Math.round(data.first * 100) / 100  +
+                    "</td><td><img class=\"spark\" src=\"http://localhost:8182" + data.spark + "\" />" +  
+                    "</td><td>" + Math.round(data.last * 100) / 100  + 
+                    "</td><td>" + Math.round(data.low * 100) / 100 + 
+                    "</td><td>" + Math.round(data.high * 100) / 100 + "</td></tr>";
+
+                $("#" + tableId + " > tbody:last").append(toAppend);
+            }
+            $(".tip").tooltip(app.tooltipOptions);
+    });
+}
+
 function addProjectToComparison(projectid, projectname) {
 	"use strict";
 	var projects = $.cookie("cccc");
