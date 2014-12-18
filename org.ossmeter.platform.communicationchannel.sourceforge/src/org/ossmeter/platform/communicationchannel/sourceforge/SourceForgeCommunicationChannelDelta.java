4d774ac93baa3a511c948d73826d9587b57c996f
Yannis Korkontzelos <ioannis.korkontzelos@manchester.ac.uk>
2014-12-18 16:11:21 +0000

    Merged

---
+++ org.ossmeter.platform.communicationchannel.sourceforge/src/org/ossmeter/platform/communicationchannel/sourceforge/SourceForgeCommunicationChannelDelta.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2014 OSSMETER Partners.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Yannis Korkontzelos - Implementation.
+ *******************************************************************************/
+package org.ossmeter.platform.communicationchannel.sourceforge;
+
+import org.ossmeter.platform.delta.communicationchannel.CommunicationChannelDelta;
+
+public class SourceForgeCommunicationChannelDelta extends CommunicationChannelDelta {
+    private static final long serialVersionUID = 1L;
+
+}
