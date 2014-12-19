6d90b4c5ae09fc876cc5ded5cb79888f9ccc5023
James Williams <jamesrobertwilliams@gmail.com>
2014-12-19 21:08:43 +0000

    Fixed project importer (client API) connection to the platform.

--- web/org.ossmeter.platform.client.api/src/org/ossmeter/platform/client/api/ProjectImportResource.java
+++ web/org.ossmeter.platform.client.api/src/org/ossmeter/platform/client/api/ProjectImportResource.java
@@ -11,7 +11,10 @@
 package org.ossmeter.platform.client.api;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 
+import org.ossmeter.platform.Configuration;
+import org.ossmeter.platform.Platform;
 import org.ossmeter.repository.model.Project;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
@@ -21,9 +24,11 @@ import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mongodb.DBObject;
+import com.mongodb.Mongo;
 
 public class ProjectImportResource extends ServerResource {
 
@@ -38,8 +43,19 @@ public class ProjectImportResource extends ServerResource {
 
 			url = url.replace("\"", "");
 			
+			Mongo mongo;
+			try {
+				mongo = Configuration.getInstance().getMongoConnection();
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+				getResponse().setStatus(Status.SERVER_ERROR_INTERNAL);
+				return Util.generateErrorMessageRepresentation(generateRequestJson(mapper, null), "The API was unable to connect to the database.");
+			}
+			Platform platform = new Platform(mongo);
+			
 			ProjectImporter importer = new ProjectImporter();
-			Project p = importer.importProject(url);
+			
+			Project p = importer.importProject(url, platform);
 			
 			if (p == null) {
 				ObjectNode msg = mapper.createObjectNode();
@@ -78,4 +94,10 @@ public class ProjectImportResource extends ServerResource {
 	
 	@JsonFilter("foofilter")
 	class Foo {}
+	
+	protected JsonNode generateRequestJson(ObjectMapper mapper, String projectName) {
+		ObjectNode n = mapper.createObjectNode();
+		n.put("project", projectName);
+		return n;
+	}
 }
