f479c18c9e36d59ee515c987a750ec5c08145679
James Williams <jamesrobertwilliams@gmail.com>
2015-02-09 14:25:08 +0000

    Removed hello world prints outs...

--- platform-extensions/org.ossmeter.platform.bugtrackingsystem.sourceforge/src/org/ossmeter/platform/bugtrackingsystem/sourceforge/Activator.java
+++ platform-extensions/org.ossmeter.platform.bugtrackingsystem.sourceforge/src/org/ossmeter/platform/bugtrackingsystem/sourceforge/Activator.java
@@ -20,7 +20,6 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
 	}
 	
 	/*
@@ -28,7 +27,6 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye World!!");
 	}
 
 }
